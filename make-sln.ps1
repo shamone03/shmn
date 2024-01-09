405d53a131d45d7a22d6315ec791c364b4b5b075
Aryah Kannan <68197305+shamone03@users.noreply.github.com>
2024-01-09 19:36:15 -0400

    changed proj name

---
+++ make-sln.ps1
@@ -0,0 +1,19 @@
+# script to build and open output project with personal profile functions
+function quiet-remove($item) {
+    if (Test-Path $item) {
+        rm -r -force $item
+        echo "Removed $item "
+    } else {
+        echo "$item does not exist"
+    }
+}
+
+quiet-remove CMakeUserPresets.json
+quiet-remove ./build/*
+conan install . --build=missing
+quiet-remove .\build\CMakeCache.txt
+quiet-remove .\build\CMakeFiles\ -r
+cmake --preset conan-default
+cd build
+cmake --build . --config Release
+cmake --open .
--- setup-proj.ps1
+++
@@ -1,9 +0,0 @@
-# script to build and open output project with personal profile functions
-cmake-clean
-conan install . --build=missing
-quiet-rm .\build\CMakeCache.txt
-quiet-rm .\build\CMakeFiles\ -r
-cmake --preset conan-default
-cd build
-cmake --build . --config Release
-cmake --open .

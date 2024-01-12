0143b79d586a473faa6366bf93c52389c6fdba5d
Aryah Kannan <68197305+shamone03@users.noreply.github.com>
2024-01-12 10:53:46 -0400

    input

--- shmn/sources/main.cpp
+++ shmn/sources/main.cpp
@@ -12,6 +12,15 @@
 #include "utils/utils.h"
 #include "window/window.h"
 
+void process_input(GLFWwindow* window, bool& fill) {
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+		glfwSetWindowShouldClose(window, true);
+	}
+	if (glfwGetKey(window, GLFW_KEY_P) == GLFW_PRESS) {
+		fill = !fill;
+		!fill ? glPolygonMode(GL_FRONT_AND_BACK, GL_LINE) : glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+	}
+}
 void shmn::examples::draw_3d(GLFWwindow* window) {
 	const auto shader = shmn::shader::shader("3d_vert.glsl", "3d_frag.glsl");
 	std::vector<std::pair<GLuint, std::string>> textures = shmn::utils::get_textures({ "saul.jpg" });
@@ -77,25 +86,27 @@ void shmn::examples::draw_3d(GLFWwindow* window) {
 	}
 
 	glEnable(GL_DEPTH_TEST);
-	
+
 	float delta = 0;
 	float time = 0;
 
 	shmn::transform::transform model;
 	// model.rotate(-45.f, {1, 0, 0});
-	model.scale({1.25f, 1.25f, 1.25f});
+	model.scale({1.f, 1.f, 1.f});
 	
 	glm::mat4 view(1.f);
-	view = glm::translate(view, {0, 0, -3.f});
+	view = glm::translate(view, {0, 0, 0.f});
 
 	glm::mat4 projection(1.f);
-	projection = glm::perspective(glm::radians(45.f), 1920.f / 1080.f, .1f, 100.f);
+	projection = glm::perspective(glm::radians(180.f), 1600.f / 900.f, .1f, 100.f);
 
 	shader.use();
 	for (int i = 0; i < textures.size(); i++) {
 		shader.set_int(textures[i].second, i);
 	}
+	bool fillMode = true;
 	while (!glfwWindowShouldClose(window)) {
+		process_input(window, fillMode);
 		const auto start = glfwGetTime();
 		glClearColor(.5f, .5f, .5f, 1.f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -115,7 +126,7 @@ void shmn::examples::draw_3d(GLFWwindow* window) {
 		glBindVertexArray(vaoID);
 		
 		model.rotate(90 * delta, { 0, 1, 0});
-		model.translate({0, std::sin(time) * .5f * delta, 0});
+
 		shader.set_mat("model", model.get_transform());
 		
 		glDrawArrays(GL_TRIANGLES, 0, 36);
@@ -131,7 +142,7 @@ void shmn::examples::draw_3d(GLFWwindow* window) {
 }
 
 int main() {
-	const auto window = shmn::window::initialize_window(1920, 1080, "saul");
+	const auto window = shmn::window::initialize_window(1600, 900, "saul");
 	std::cout << "OpenGL Version: " << glGetString(GL_VERSION) << std::endl;
 	
 	shmn::examples::draw_3d(*window);

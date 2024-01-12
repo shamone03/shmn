25a057b375430a0f6debfe4bc138542a81c1452e
Aryah Kannan <68197305+shamone03@users.noreply.github.com>
2024-01-11 20:49:56 -0400

    simplified

--- shmn/resources/shaders/3d_vert.glsl
+++ shmn/resources/shaders/3d_vert.glsl
@@ -1,12 +1,14 @@
 ﻿
 #version 460 core
 layout (location = 0) in vec3 pos;
-layout (location = 1) in vec3 color;
 layout (location = 2) in vec2 textCoord;
 
 out vec2 vertTextCoord;
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 projection;
 
 void main() {
-    gl_Position = vec4(pos, 1.0f);
+    gl_Position = projection * view * model * vec4(pos, 1.0f);
     vertTextCoord = textCoord;
 }
--- shmn/sources/main.cpp
+++ shmn/sources/main.cpp
@@ -18,17 +18,16 @@ void shmn::examples::draw_3d(GLFWwindow* window) {
 
 	constexpr float vertices[] {
 		// positions          // colors           // texture coords
-		1.f,  1.f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // top right
-		1.f, -1.f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // bottom right
-	   -1.f, -1.f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // bottom left
-	   -1.f,  1.f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // top left 
+		.5f,  .5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // top right
+		.5f, -.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // bottom right
+	   -.5f, -.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // bottom left
+	   -.5f,  .5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // top left 
    };
 
 	constexpr GLuint indices[] {
 		0, 1, 2,
 		2, 3, 0
 	};
-	std::cout << textures.size() << std::endl;
 	GLuint vboID, eboID, vaoID;
 	{
 		glGenBuffers(1, &vboID);
@@ -54,26 +53,39 @@ void shmn::examples::draw_3d(GLFWwindow* window) {
 
 	float delta;
 	float sum = 0;
+
+	shmn::transform::transform model;
+	model.rotate(-20.f, {1, 0, 0});
+
+	glm::mat4 view(1.f);
+	view = glm::translate(view, {0, 0, -3.f});
+
+	glm::mat4 projection(1.f);
+	projection = glm::perspective(glm::radians(45.f), 800.f / 600.f, .1f, 100.f);
+
+	int i = 0;
+	shader.use();
+	for (const auto& [id, name] : textures) {
+		shader.set_int(name, i);
+		glActiveTexture(GL_TEXTURE0 + i);
+		glBindTexture(GL_TEXTURE_2D, id);
+		i++;
+	}
 	
 	while (!glfwWindowShouldClose(window)) {
 		const auto start = glfwGetTime();
 		glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 		
-		shader.use();
-		int i = 0;
-		
-		for (const auto& [id, name] : textures) {
-			shader.use();
-			shader.set_int(name, i);
-			
-			glActiveTexture(GL_TEXTURE0 + i);
-			glBindTexture(GL_TEXTURE_2D, id);
+		// shader.use();
+		shader.set_mat("projection", projection);
+		shader.set_mat("view", view);
 
-			glBindVertexArray(vaoID);
-			glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, NULL);
-			i++;
-		}
+		model.rotate(90 * delta, { 0, 1, 0});
+		shader.set_mat("model", model.get_transform());
+		
+		glBindVertexArray(vaoID);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, NULL);
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
